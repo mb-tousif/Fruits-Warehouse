61f90ae8c68866da23a2028e7415b326ab0a6575
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-07 13:56:20 +0600

    Restock and Delevered Function implemented

--- src/Pages/UpdateFruit/UpdateFruit.js
+++ src/Pages/UpdateFruit/UpdateFruit.js
@@ -8,8 +8,8 @@ const UpdateFruit = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
   const { name, img, description, price, quantity, supplier, sold } = item;
-  // const [newQuantity, setNewQuantity] = useState(quantity);
   const [loader, setLoader] = useState(false);
+  const [number, setNumber] = useState("")
 
   const url = `https://fruits-warehouse-server.vercel.app/fruit/${id}`;
   useEffect(() => {
@@ -24,64 +24,65 @@ const UpdateFruit = () => {
     return <DataLoader />;
   }
 
-  // //* =========== deliver funcion =====================
-  // const handleDeliver = () => {
-  //   const newQuantity = Number(quantity) - 1;
-  //   console.log(newQuantity);
-  //   const data = { quantity: newQuantity };
-  //   console.log(data);
-  //   //* send data to the server
-  //   fetch(url, {
-  //     method: "PUT",
-  //     headers: {
-  //       "content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(data),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log("success", data);
-  //       //* load updated data
-  //       setLoader(true);
-  //       fetch(url)
-  //         .then((res) => res.json())
-  //         .then((data) => {
-  //           setItem(data);
-  //           setLoader(false);
-  //         });
-  //     });
-  // };
+  //* =========== deliver funcion =====================
+  const handleDeliver = (e) => {
+    e.preventDefault();
+    const newQuantity = Number(quantity) - 1;
+    console.log(newQuantity);
+    const data = { quantity: newQuantity };
+    console.log(data);
+    //* send data to the server
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("success", data);
+        //* load updated data
+        setLoader(true);
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setItem(data);
+            setLoader(false);
+          });
+      });
+  };
 
-  // //* =============== restock function ================
-  // const handleAdd = (e) => {
-  //   e.preventDefault();
-  //   const number = e.target.number.value;
-  //   console.log(number);
-  //   const newQuantity = Number(quantity) + Number(number);
-  //   const data = { quantity: newQuantity };
-  //   console.log(data);
-  //   //* send data to the server
-  //   fetch(url, {
-  //     method: "PUT",
-  //     headers: {
-  //       "content-type": "application/json",
-  //     },
-  //     body: JSON.stringify(data),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log("success", data);
-  //       e.target.reset();
-  //       //* load updated data
-  //       setLoader(true);
-  //       fetch(url)
-  //         .then((res) => res.json())
-  //         .then((data) => {
-  //           setItem(data);
-  //           setLoader(false);
-  //         });
-  //     });
-  // };
+  //* =============== restock function ================
+  const handleAdd = (e) => {
+    e.preventDefault();
+    console.log("input ", number);
+    const newQuantity = Number(quantity) + Number(number);
+    const data = { quantity: newQuantity };
+    console.log(data);
+    //* send data to the server
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("success", data);
+        // e.target.reset();
+        //* load updated data
+        setLoader(true);
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setItem(data);
+            setLoader(false);
+            setNumber("")
+          });
+      });
+  };
 
   return (
     <section className="text-gray-50 md:w-2/3 mx-auto md:p-2 body-font">
@@ -115,7 +116,7 @@ const UpdateFruit = () => {
           </p>
           <button
             disabled={quantity < 1}
-            // onClick={handleDeliver}
+            onClick={handleDeliver}
             className="w-full md:w-3/5  px-6 py-2.5 hover:bg-[#1c3a13] bg-[#06582096] md:text-lg rounded-2xl"
           >
             Delivered
@@ -125,13 +126,15 @@ const UpdateFruit = () => {
             <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
               <input
                 type="number"
-                name="number"
+                value={number}
+                onChange={(e)=>setNumber(e.target.value)}
                 className="w-full bg-[#06582096] rounded bg-opacity-40 focus:ring-2 hover:bg-[#1c3a13] focus:border-[#06582096] text-center outline-none text-gray-100 py-1 px-3 leading-9 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <button 
-            // onClick={handleAdd} 
-            className="inline-flex hover:bg-[#1c3a13] bg-[#06582096] py-2 px-6 focus:outline-none rounded text-lg">
+            <button
+              onClick={handleAdd}
+              className="inline-flex hover:bg-[#1c3a13] bg-[#06582096] py-2 px-6 focus:outline-none rounded text-lg"
+            >
               <AiOutlinePlusCircle className="text-gray-50 mr-2 mt-1.5" />
               Restock
             </button>
