805cac1318efa6a4e2abb3c0c172cd7537dd6436
AZIZUL HAQUE TOUSIF <meta.block.tousif@gmail.com>
2022-09-08 00:26:53 +0600

    useToken created

---
+++ src/Hooks/useToken.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+const useToken = user => {
+    const [token, setToken] = useState("");
+
+    useEffect(()=>{
+        const email = user?.user?.email;
+        const currentUser = {email: email}
+        const url = `https://fruits-warehouse-server.vercel.app/api/user/${email}`;
+        if(email){
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(currentUser)
+            })
+            .then(res=> res.json())
+            .then(data=> {
+                console.log("data from token", data);
+            })
+        }
+    },[user])
+    return [token]
+};
+
+export default useToken;
